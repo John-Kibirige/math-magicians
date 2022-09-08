ce7254129374d5f8ff00d610bd65ef490f02f4b8
John Kibirige <kibirigejohn005@gmail.com>
2022-09-08 12:19:27 +0300

    Refactor Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,39 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Key from './Key';
 import calculate from '../logic/calculate';
 
-/* eslint-disable  */
-class Calculator extends React.Component {
-  constructor() {
-    super();
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    } 
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+     total: null,
+     next: null,
+    operation: null,
+  })
 
-  handleClick(keyPressed) {
-    this.setState(prevState => ({
+  const handleClick = (keyPressed) => {
+    setState(prevState => ({
       ...calculate(prevState, keyPressed)
     }))
   }
-  
-  render() {
-    const keys = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='] 
-    const page = keys.map(k => <Key value={k} key={k} handleClickEvent={this.handleClick}/>)
 
-    return (
-      <section className="overall-container">
-        <div className="display">{this.state.total ? this.state.total : '0'}</div>
-        <div className='grid-container'>
-            {page}
-        </div>        
-      </section>
+  const keys = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='] 
+  const page = keys.map(k => <Key value={k} key={k} handleClickEvent={handleClick}/>)
 
-    );
-  }
+  return (
+    <section className="overall-container">
+      <div className="display">{state.total ? state.total : '0'}</div>
+      <div className='grid-container'>
+          {page}
+      </div>        
+    </section>
+  );
 }
 
 export default Calculator;
