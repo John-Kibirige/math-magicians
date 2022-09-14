85bd1e5d86d85b616a073bf4cc351f55b8fe9c59
John Kibirige <kibirigejohn005@gmail.com>
2022-09-14 13:52:08 +0300

    Add home and calculator styles

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Quote = () => {
   return (
     <div>
-      <p>
+      <p className='quote'>
         Mathematics is not about numbers, equations, computations or algorithms:
         it is about understanding. -William Paul Thurston
       </p>
