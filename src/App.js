c12e09d25ee1712544a9093818d110a25f6e8269
John Kibirige <kibirigejohn005@gmail.com>
2022-09-14 11:53:00 +0300

    Create Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+/* eslint-disable  */
 import './App.css';
 import Calculator from './components/Calculator';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SharedComponent } from './components/SharedComponent';
 function App() {
   return (
     <div>
-      <Calculator />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SharedComponent />}>
+            <Route index element={<Home />} />
+            <Route path='calculator' element={<Calculator />} />
+            <Route path='quote' element={<Quote />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+/* eslint-disable  */
+import React from 'react';
+export const Home = () => {
+  return (
+    <div className='home-page'>
+      <h2 className='title'>Welcome to our page!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+        officiis quae laborum cum, inventore ab placeat, corporis voluptates eum
+        magnam praesentium voluptate pariatur non sit exercitationem et
+        laudantium qui ducimus tempore nulla cumque quaerat impedit? Totam ut
+        earum, nihil minus, aut itaque quam velit laborum voluptatum sunt quos
+        hic nostrum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+        officiis quae laborum cum, inventore ab placeat, corporis voluptates eum
+        magnam praesentium voluptate pariatur non sit exercitationem et
+        laudantium qui ducimus tempore nulla cumque quaerat impedit? Totam ut
+        earum, nihil minus, aut itaque quam velit laborum voluptatum sunt quos
+        hic nostrum. officiis quae laborum cum, inventore ab placeat, corporis
+        voluptates eum magnam praesentium voluptate pariatur non sit
+        exercitationem et laudantium qui ducimus tempore nulla cumque quaerat
+        impedit? Totam ut earum, nihil minus, aut itaque quam velit laborum
+        voluptatum sunt quos hic nostrum.
+      </p>
+    </div>
+  );
+};
